c088eb45e769b658830224c2099cedb604ac5d1c
vgdef <valensyagabrielledef@gmail.com>
2024-03-15 08:58:01 +0800

    8:57 AM

--- income/models.py
+++ income/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Income(models.Model):
+    kategori = models.CharField(max_length=255)
+    jumlah = models.CharField(max_length=255)
+    keterangan = models.CharField(max_length=255)
+    tanggal = models.DateTimeField(auto_now_add=True)
--- income/views.py
+++ income/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Income
 
+# Create your views here
 def income(request):
-    return HttpResponse("Hello world!")
+    myincome = Income.objects.all().values()
+    template = loader.get_template('income.html')
+    context = {
+        'myincome': myincome,
+    }
+    
+    return HttpResponse(template.render(context, request))
